d6b75596f9101073353fc9d83b515dd94c64e0b4
MattiaPannone <82405725+MattiaPannone@users.noreply.github.com>
2022-09-25 20:40:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 //main.js
 import * as THREE from 'three';
 import Stats from 'three/addons/libs/stats.module.js';
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { ArcballControls } from 'three/addons/controls/ArcballControls.js';
-import { DragControls } from 'three/addons/controls/DragControls.js';
-import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
 import { Model3D } from '/worlds_elements/model3D.js';
 import { World0 } from '/worlds/world0.js';
 import { World1 } from '/worlds/world1.js';
