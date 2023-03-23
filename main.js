ca14dca8ddbc60eac76f3470c8afb377e2c3bf84
Mattia Pannone <128706311+PannonsCode@users.noreply.github.com>
2023-03-23 19:58:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 import * as THREE from 'three';
 import Stats from 'three/addons/libs/stats.module.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { Model3D } from 'w_e/model3D.js';
-import { World0 } from 'w/world0.js';
-import { World1 } from 'w/world1.js';
-import { World2 } from 'w/world2.js';
-import { World3 } from 'w/world3.js';
-import { World4 } from 'w/world4.js';
+import { Model3D } from '/w_e/model3D.js';
+import { World0 } from '/w/world0.js';
+import { World1 } from '/w/world1.js';
+import { World2 } from '/w/world2.js';
+import { World3 } from '/w/world3.js';
+import { World4 } from '/w/world4.js';
 import { Spiderman } from '/spiderman.js';
 import * as TWEEN from './tween_js/tween.esm.js';
 import {Util} from "/utils.js";
